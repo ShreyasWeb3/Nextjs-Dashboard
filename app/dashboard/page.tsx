5d947c4523fce920d6dead13a58d2c8902ffbe8a
ShreyasWeb3 <shreyasweb3@gmail.com>
2024-02-19 22:27:56 +0530

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen text-6xl font-bold flex items-center justify-center'>Dashboard Page</div>
+  )
+}
+
+export default page
